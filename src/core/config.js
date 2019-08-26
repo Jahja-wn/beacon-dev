f31ae233ad3b79fafb0c14fb377da137b46632d5
jaja_pp <59011178@kmitl.ac.th>
2019-08-26 17:18:43 +0700

    change uri & package.json

--- src/core/config.js
+++ src/core/config.js
@@ -5,8 +5,8 @@ const uri=process.env.MONGODB_URL;
 module.exports = {
     uri,
     port,
-    channelAccessToken: process.env.channelAccessToken,
-    channelSecret: process.env.channelSecret,
+    channelAccessToken: "azyN3SbF0UaXDTeN0IMXwbc5HvDxq7HnuM8Z/YHenOlq3KbblMmR5wYa+mY5ZTUyIAKG1PVlEXVQwev6GA51KrSxAFWg9VX2XkuMV66is0G3B7fotyOHfU8TuMoehB96vtrqhHX0hWjOBDCp5y2FGwdB04t89/1O/w1cDnyilFU=",
+    channelSecret: "b01c41c0e015e2d56e56edb0f9b02860",
     ReportGroupId: "C8fbee37d52c847f6aa8e2a22950e96be",
     AnswerAlertDuration: 5000, //ms,
     ElasticConfig: {
