475e9712b34f3f72821368e41558b5b5c0510686
jaja_pp <59011178@kmitl.ac.th>
2019-08-30 13:56:12 +0700

    edit history template

--- src/app.js
+++ src/app.js
@@ -43,9 +43,9 @@ app.post('/submit', (req, res) => {
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/history', middleware(config),function (req, res) {
+app.get('/history', function (req, res) {
 
-  dal.find({ userId:event.source.userId}, activityColl)
+  dal.find({ type: "in" }, activityColl)
     .then((docs) => {
       res.render('history', { docs: docs });
       //  res.send(docs)
