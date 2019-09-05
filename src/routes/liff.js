b2cc2127485d3b666bec2643045b0b12d237541d
jaja_pp <59011178@kmitl.ac.th>
2019-09-05 16:17:03 +0700

    edit style of history

--- src/routes/liff.js
+++ src/routes/liff.js
@@ -37,11 +37,10 @@ router.post('/gethistory', function (req, res) {
     var getuserid = req.body;
     dal.find(getuserid, activityColl)
         .then((docs) => {
-            let users = '<tr><th>User</th><thead><th>name</th><thead><th>type</th><th>date/time</th><th>location</th><th>plan</th></tr>';
+            let users = ' <thead><tr><th>name</th><thead><th>type</th><th>date/time</th><th>location</th><th>plan</th></tr></thead><tbody>';
             docs.forEach(doc => {
-                users += '<tr><td>' + doc.url+ '</td><td>' + doc.displayName + '</td><td>' + doc.type + '</td><td>' + doc.timestamp + '</td><td>' + doc.location.locationName + '</td><td>' + doc.plan + '</td>'
+                users += '<tr><td>' + doc.url+ '</td><td>' + doc.displayName + '</td><td>' + doc.type + '</td><td>' + doc.timestamp + '</td><td>' + doc.location.locationName + '</td><td>' + doc.plan + '</td></tbody>'
             });
-            // users += ''
             res.status(200).send(users)
         })
         .catch((err) => {
