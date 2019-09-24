250d0c3f5c0556d3d18c21a3418e46731af82c43
jaja_pp <59011178@kmitl.ac.th>
2019-09-24 10:16:00 +0700

    mapping data on elasticsearch

--- src/core/service/elastic_service.js
+++ src/core/service/elastic_service.js
@@ -1,111 +1,125 @@
 import elasticsearch from '@elastic/elasticsearch';
-import { finalConfig } from  '../../../config';
-import { activities , users} from '../model';
+import { finalConfig } from '../../../config';
+import { activities, users } from '../model';
 import { logger } from '../../logger';
 
 const current_datetime = new Date();
 const activityMapping = {
-    "properties": {
-        "timestamp": {
-        "type": "date"
-        },
-        "location":{
-            "properties":{
-                "point":{
-                    "type":"geo_point"
+        "properties": {
+            "timestamp": {
+                "type": "date"
+            },
+            "location": {
+                "properties": {
+                    "point": {
+                        "properties": {
+                            "coordinates": {
+                                "type": "geo_point"
+                            }
+                        }
+                    }
                 }
             }
-        }
+        
     }
 };
 
-const client = new elasticsearch.Client({node:finalConfig.elasticConfig});
+const client = new elasticsearch.Client({ node: finalConfig.elasticConfig });
 
-async function insertActivity(activity){
+async function insertActivity(activity) {
     let gp7Date = new Date(activity.timestamp);
-    let indexStr = `activity-${gp7Date.getDate()}-${gp7Date.getMonth()+1}-${gp7Date.getFullYear()}`;
+    let indexStr = `activity-${gp7Date.getDate()}-${gp7Date.getMonth() + 1}-${gp7Date.getFullYear()}`;
     let haveIndex = false;
-    try{
-        if(!await client.indices.exists({index: indexStr})){
+    try {
+        const indexExists = await client.indices.exists({ index: indexStr })
+        haveIndex = indexExists.body
+        console.log("body", indexExists.body)
+        if (!haveIndex) {
             logger.debug(`try create index: [${indexStr}]`);
-            await client.indices.create({index: indexStr,body: {mappings:activityMapping}});
+            await client.indices.create({ index: indexStr, body: {
+                mappings:activityMapping,
+                settings: {}
+            } });
             logger.info(`create index: [${indexStr}] success`);
+
+            haveIndex = true;
+
         }
-        haveIndex = true;
     }
-    catch(err){
-        if(err.body !== undefined && err.body.error !== undefined){
-            if(err.body.error.type === 'resource_already_exists_exception'){
+    catch (err) {
+        if (err.body !== undefined && err.body.error !== undefined) {
+            if (err.body.error.type === 'resource_already_exists_exception') {
                 haveIndex = true;
                 logger.debug(`create index: [${indexStr}] already exist`);
             }
-            else{
-                logger.info("cannot create index: ",err.body.error.type);
+            else {
+                logger.info("cannot create index: ", err.body.error.type);
             }
         }
-        else{
-            logger.error("cannot create index:"+ err);
+        else {
+            logger.error("error is undefined:" + err);
         }
-        
+
     }
-    if(haveIndex){
-        try{
+    if (haveIndex) {
+        try {
             await client.index({
                 index: indexStr,
                 type: '_doc',
                 refresh: true,
                 body: activity
             });
-            logger.debug("insert activity: "+JSON.stringify(activity)+" success");
+            logger.debug("insert activity: " + JSON.stringify(activity) + " success");
         }
-        catch(err){
+        catch (err) {
+            console.log("from elastic",JSON.stringify(err))
             logger.error("cannot insert activity: ",err.body.error.type);
         }
     }
-    
+
 }
 
 
-function elastic_update(obj , target) {
+function elastic_update(obj, target) {
     var presentIndex;
     if (obj instanceof activities) {
-        presentIndex = 'activity-'+current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getFullYear();
+        presentIndex = 'activity-' + current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getFullYear();
     } else if (obj instanceof users) {
         presentIndex = "user";
     }
     var queryArray = [];
-    for(var property in obj){
-        if(obj[property] != null && property != target){
-            queryArray.push({match : { [property] : obj[property] }});
+    for (var property in obj) {
+        if (obj[property] != null && property != target) {
+            queryArray.push({ match: { [property]: obj[property] } });
         }
     }
-    var scriptSet = {"inline": `ctx._source.${target} = '${obj[target]}'; `};
-    if(obj[target] == true || obj[target] == false) scriptSet = {"inline": `ctx._source.${target} = ${obj[target]}; `};
-    
+    var scriptSet = { "inline": `ctx._source.${target} = '${obj[target]}'; ` };
+    if (obj[target] == true || obj[target] == false) scriptSet = { "inline": `ctx._source.${target} = ${obj[target]}; ` };
+
     var promise = new Promise((resolve, reject) => {
-        
+
         var res = client.updateByQuery({
             index: presentIndex,
-            refresh : true,
+            refresh: true,
             type: '_doc',
             body: {
                 "query": {
                     "bool": {
-                      "must": queryArray
+                        "must": queryArray
                     }
-                  },
+                },
                 "script": scriptSet
             }
         })
-        
-     
+
+
         resolve(res);
         reject();
-     
-        
+
+
     });
 
-  
+
     return promise;
 }
 
